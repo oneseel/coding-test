46b14c6d71c8d4937498831aae7ec925cf1606f1
이선오 <140589023+oneseel@users.noreply.github.com>
2024-03-19 20:21:24 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 86.77 ms, Memory: 95.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+        String answer = "";
+        
+        for (String player : participant) {
+            map.put(player, map.getOrDefault(player, 0) + 1);
+        }
+        for (String player : completion) {
+            map.put(player, map.get(player) - 1);
+        }
+        
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0) {
+                answer = key;
+            }
+        }
+        return answer;
+    }
+}
