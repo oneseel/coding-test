d686cd029fadc91264f0c836607fe688ddbab531
이선오 <140589023+oneseel@users.noreply.github.com>
2024-03-22 18:58:27 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 0.03 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int solution(int[] arrayA, int[] arrayB) {
+        
+        int gcdA = arrayA[0];
+        for (int i = 1; i < arrayA.length; i++) {
+            gcdA = gcd(gcdA, arrayA[i]);
+        }
+        
+        int gcdB = arrayB[0];
+        for (int i = 1; i < arrayB.length; i++) {
+            gcdB = gcd(gcdB, arrayB[i]);
+        }
+        
+        boolean checkA = check(gcdA, arrayB);
+        boolean checkB = check(gcdB, arrayA);
+        
+        if (checkA && checkB) {
+            return Math.max(gcdA, gcdB);
+        }
+        
+        if (checkA) return gcdA;
+        if (checkB) return gcdB;
+        
+        return 0;
+    }
+
+    // 최대공약수(GCD)
+    private int gcd(int a, int b) {
+        if (a % b == 0) {
+            return b;
+        }
+        return gcd(b, a % b);
+    }
+    
+    private boolean check(int gcd, int[] arr) {
+        for (int num : arr) {
+            if (num % gcd == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
