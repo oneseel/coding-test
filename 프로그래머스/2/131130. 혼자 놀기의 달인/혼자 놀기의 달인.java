270b71b44ad2247af5b1a91d54c2ba0e471f0092
이선오 <140589023+oneseel@users.noreply.github.com>
2024-04-01 14:24:53 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.29 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int[] cards) {
+        boolean[] visited = new boolean[cards.length];
+        List<Integer> group = new ArrayList<>();
+
+        for (int i = 0; i < cards.length; i++) {
+            if (!visited[i]) {
+                int currentBox = i;
+                int count = 0;
+                
+                while (!visited[currentBox]) {
+                    visited[currentBox] = true;
+                    currentBox = cards[currentBox] - 1;
+                    count++;
+                }
+                
+                group.add(count);
+            }
+        }
+        
+        group.sort(Comparator.reverseOrder());
+        
+        if (group.size() < 2) {
+            return 0;
+        } else {
+            return group.get(0) * group.get(1);
+        }
+    }
+}
