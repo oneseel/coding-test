d4aad33960da5e8a56d959247a185954b1a7b3bf
이선오 <140589023+oneseel@users.noreply.github.com>
2024-04-05 14:22:27 +0900

    [level 2] Title: 우박수열 정적분, Time: 9.76 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+class Solution {
+    public double[] solution(int k, int[][] ranges) {
+        
+        // 우박수열 만들기
+        List<Integer> list = new ArrayList<>();
+        
+        while (k != 1) {
+            list.add(k);
+            if (k % 2 == 0) {
+                k = k / 2;
+            } else {
+                k = (k * 3) + 1;
+            }
+        }
+        list.add(1);
+        System.out.println(list);
+        
+        // 각 넓이 구하기
+        List<Double> areaList = new ArrayList<>();
+        for (int i = 0; i < list.size() - 1; i++) {
+            double area = ((double) list.get(i) + (double) list.get(i + 1)) / 2.0;
+            areaList.add(area);
+        }
+        System.out.println(areaList);
+        
+        // 부분구간 정적분 값 구하기
+        double[] answer = new double[ranges.length];
+        int n = list.size() - 1;
+        
+        for (int i = 0; i < ranges.length; i++) {
+            int a = ranges[i][0];
+            int b = ranges[i][1];
+            
+            int start = a;
+            int end = n + b;
+            
+            if (start > end) {
+                answer[i] = -1.0;
+                continue;
+            }
+            
+            double sum = 0;
+            for (int j = start; j < end; j++) {
+                sum += areaList.get(j);
+            }
+            
+            answer[i] = sum;
+        }
+        
+        return answer;
+    }
+}
