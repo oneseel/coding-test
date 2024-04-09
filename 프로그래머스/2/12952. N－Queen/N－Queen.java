c39b39c96d2e54cf90f944f58bd114a7db69177d
이선오 <140589023+oneseel@users.noreply.github.com>
2024-04-09 22:19:01 +0900

    [level 2] Title: N-Queen, Time: 168.74 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {      
+    int[] board;
+    int count = 0;
+    
+    public int solution(int n) {
+        board = new int[n];
+        backTracking(n, 0);
+        
+        return count;
+    }
+    
+    private void backTracking(int n, int row){
+        if(row == n){
+            count++;
+            return;
+        }
+        
+        for(int i = 0; i < n; i++){
+            board[row] = i;          
+            if(possible(row)){
+                backTracking(n, row+1);
+            }
+        }        
+    }
+    
+    private boolean possible(int row){
+        for(int i = 0; i < row; i++){
+            if(board[i] == board[row]) return false;     
+            if(Math.abs(row - i) == Math.abs(board[row] - board[i])) return false; 
+        }
+        return true;
+    }
+}
