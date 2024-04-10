eea7984e1c55f0e9c4a8e28eda0b6f40d5610984
이선오 <140589023+oneseel@users.noreply.github.com>
2024-04-10 22:35:05 +0900

    [Bronze V] Title: 꼬마 정민, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+        
+        System.out.println(a + b + c);
+    }
+}
