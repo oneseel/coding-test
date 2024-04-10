dd12cbea31a53ce2ee1a427c3e7d6f2a35403f77
이선오 <140589023+oneseel@users.noreply.github.com>
2024-04-10 21:41:03 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String name = bf.readLine();
+        System.out.println(name + "??!");
+    }
+}
