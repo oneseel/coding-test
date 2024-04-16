c105cf2a571e5358681a7c3090f087f1c131c4f1
이선오 <140589023+oneseel@users.noreply.github.com>
2024-04-16 17:12:44 +0900

    [Bronze V] Title: A+B - 5, Time: 132 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int sum = 0;
+        
+        while(true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a == 0 && b == 0) {
+                break;
+            }
+            sum = a + b;
+            System.out.println(sum);
+        }
+    }
+}
