f12f50dbb76b5ec37ab68bbbdbc7661ca1c030ee
이선오 <140589023+oneseel@users.noreply.github.com>
2024-04-16 10:23:48 +0900

    [level 2] Title: 요격 시스템, Time: 0.49 ms, Memory: 67 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] targets) {
+        int prev = 0;
+        int count = 0;
+        
+        // e를 기준으로 내림차순 정렬(같으면 s를 기준으로 오름차순)
+        Arrays.sort(targets, (o1, o2) -> 
+            (o1[1] == o2[1]) ? o1[0] - o2[0] : o1[1] - o2[1]
+        );
+
+        for (int i = 0; i < targets.length; i++) {
+            int s = targets[i][0];
+            int e = targets[i][1];
+            if (prev <= s) {
+                prev = e;
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
