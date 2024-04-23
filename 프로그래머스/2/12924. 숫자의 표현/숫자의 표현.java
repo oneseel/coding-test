114970a451ed5675eb474de4af0ac964ccd94aca
이선오 <140589023+oneseel@users.noreply.github.com>
2024-04-23 11:00:13 +0900

    [level 2] Title: 숫자의 표현, Time: 1.11 ms, Memory: 67 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,9 +1,17 @@
-public class Solution {
+class Solution {
     public int solution(int n) {
-        int count = 0;
-        for (int k = 1; k * (k + 1) <= 2 * n; k++) {
-            if ((n - (k * (k + 1) / 2)) % k == 0) count++;
+        int count = 1;
+        
+        for (int i = 1 ; i <= n; i++) {
+            int temp = i;
+            for (int j = i; temp <= n; j++) {
+                temp += (j + 1);
+                if (temp == n) {
+                    count++;
+                }
+            }
         }
+        
         return count;
     }
 }
