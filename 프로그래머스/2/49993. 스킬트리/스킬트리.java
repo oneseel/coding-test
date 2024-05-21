b6a8efddf2b0c05e33a3ebc8f4e7fe996fd44c9b
이선오 <140589023+oneseel@users.noreply.github.com>
2024-05-21 10:12:53 +0900

    [level 2] Title: 스킬트리, Time: 0.04 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int count = 0;
+        for (String s : skill_trees) {
+            if (isVaild(skill, s)) count++;
+        }
+        return count;
+    }
+    
+    private static boolean isVaild(String skill, String s) {
+        int idx = 0; // 스킬순서
+        
+        for(char c : s.toCharArray()) {
+            // 스킬트리에 있는 값(temp)이 스킬에 있는지 확인
+            int temp = skill.indexOf(c);
+            
+            // 없다면 넘어간다.
+            if (temp == -1) continue;
+            
+            // 있지만 현재순서가 아니면 false
+            if (temp != idx) return false;
+            idx++;
+        }
+        return true;
+    }
+}
