93baa7f66315f40743b1ed218dba34d689d67d35
이선오 <140589023+oneseel@users.noreply.github.com>
2024-05-29 13:41:03 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.52 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,65 +1,66 @@
 import java.util.*;
 
 class Solution {
-    static boolean[] visit;
-    static List<List<Integer>> tree;
+    private static List<List<Integer>> tree;
+    private static boolean[] visited;
+    private static int v1, v2, count;
     
     public int solution(int n, int[][] wires) {
         
-        // 트리 만들기
+        // 트리 구성
         tree = new ArrayList<>();
-        
+
         for (int i = 0; i <= n; i++) {
             tree.add(new ArrayList<>());
         }
         
         for (int[] wire : wires) {
-            int node1 = wire[0];
-            int node2 = wire[1];
-            tree.get(node1).add(node2);
-            tree.get(node2).add(node1);
+            v1 = wire[0];
+            v2 = wire[1];
+            // 각 노드 연결
+            tree.get(v1).add(v2);
+            tree.get(v2).add(v1); 
         }
         
         int min = Integer.MAX_VALUE;
         
         for (int[] wire : wires) {
-            int node1 = wire[0];
-            int node2 = wire[1];
-
-            // 노드 연결 제거
-            tree.get(node1).remove(Integer.valueOf(node2));
-            tree.get(node2).remove(Integer.valueOf(node1));
+            v1 = wire[0];
+            v2 = wire[1];
+            // 각 노드 끊기
+            tree.get(v1).remove(Integer.valueOf(v2));
+            tree.get(v2).remove(Integer.valueOf(v1));
             
-            visit = new boolean[n + 1];
-            int count = bfs(node1);
+            visited = new boolean[n + 1];
+            count = bfs(v1);
             
-            min = Math.min(min, Math.abs((n - count) - count));
+             min = Math.min(min, Math.abs((n - count) - count));
             
-            // 노드 다시 연결
-            tree.get(node1).add(node2);
-            tree.get(node2).add(node1);
+            // 재연결
+            tree.get(v1).add(v2);
+            tree.get(v2).add(v1);
         }
         
         return min;
     }
     
-    public int bfs(int node) {
+    private static int bfs(int start) {
         Queue<Integer> queue = new LinkedList<>();
-        queue.offer(node);
-        visit[node] = true;
-        int count = 1;
-
+        queue.offer(start);
+        visited[start] = true;
+        count = 1;
+        
         while (!queue.isEmpty()) {
             int temp = queue.poll();
+            
             for (int i : tree.get(temp)) {
-                if (!visit[i]) {
+                if (!visited[i]) {
                     queue.offer(i);
-                    visit[i] = true;
+                    visited[i] = true;
                     count++;
                 }
             }
         }
-
         return count;
     }
 }
