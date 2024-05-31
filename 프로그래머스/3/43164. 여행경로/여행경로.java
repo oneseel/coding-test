47d3f5003052dbb82939c2850277bc9f21ddbeda
이선오 <140589023+oneseel@users.noreply.github.com>
2024-05-31 13:05:05 +0900

    [level 3] Title: 여행경로, Time: 10.96 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Route {
+    String start;
+    String end;
+    
+    public Route (String start, String end) {
+        this.start = start;
+        this.end = end;
+    }
+}
+
+class Solution {
+    private static List<String> route; // 이동경로 리스트
+    private static boolean[] visited; // 경로 방문 배열
+    private static List<String> answer;
+    
+    public String[] solution(String[][] tickets) {
+        route = new ArrayList<>();
+        answer = new ArrayList<>();
+        visited = new boolean[tickets.length];
+        
+        // 티켓 정보를 정렬 (사전순으로 경로를 반환하기 위해)
+        Arrays.sort(tickets, (a, b) -> a[1].compareTo(b[1]));
+        
+        dfs("ICN", "ICN", tickets, 0);
+        
+        return answer.get(0).split(" ");
+    }
+    
+    private void dfs(String current, String route, String[][] tickets, int depth) {
+        if (depth == tickets.length) {
+            answer.add(route);
+            return;
+        }
+        
+        for (int i = 0; i < tickets.length; i++) {
+            if (!visited[i] && tickets[i][0].equals(current)) {
+                visited[i] = true;
+                dfs(tickets[i][1], route + " " + tickets[i][1], tickets, depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
