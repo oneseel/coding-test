70ac5d3fad51edd419d1b822c4619c74fa795bc8
이선오 <140589023+oneseel@users.noreply.github.com>
2024-07-06 15:23:40 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1608 ms, Memory: 62796 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -5,31 +5,33 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        
-        int[] number = new int[N]; // 입력된 배열
-        int[] prefixSum = new int[N]; // 부분합을 저장할 배열
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        int[] sum = new int[n];
         
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            number[i] = Integer.parseInt(st.nextToken());
-            if (i == 0) prefixSum[i] = number[i]; // 첫 번째 원소는 그대로 할당
-            else prefixSum[i] = prefixSum[i - 1] + number[i]; // 이전까지의 합에 현재 값을 더함
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        sum[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            sum[i] = sum[i - 1] + arr[i];
         }
         
-        for (int k = 0; k < M; k++) {
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int i = Integer.parseInt(st.nextToken());
-            int j = Integer.parseInt(st.nextToken());
-            int sum;
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
             
-            // 시작 인덱스가 1인 경우, j까지의 합은 prefixSum[j - 1]과 같다
-            if (i == 1) sum = prefixSum[j - 1]; 
-            // 그렇지 않은 경우, i부터 j까지의 합은 prefixSum[j - 1]에서 prefixSum[i - 2]를 뺀 값과 같다
-            else sum = prefixSum[j - 1] - prefixSum[i - 2]; 
+            int result = 0;
+            if (a == 0) {
+                result = sum[b];    
+            } else {
+                result = sum[b] - sum[a - 1];
+            }
             
-            System.out.println(sum);
+            System.out.println(result);
         }
     }
 }
