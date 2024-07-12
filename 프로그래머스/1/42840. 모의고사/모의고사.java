8584e50a43c40fd9a5777277976c3d16288222b4
이선오 <140589023+oneseel@users.noreply.github.com>
2024-07-12 22:44:54 +0900

    [level 1] Title: 모의고사, Time: 4.49 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,32 +1,35 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class Solution {
-
-  public int[] solution(int[] answers) {
-    int count1 = 0;
-    int count2 = 0;
-    int count3 = 0;
-    
-    int[] supoja1Pattern = {1, 2, 3, 4, 5};
-    int[] supoja2Pattern = {2, 1, 2, 3, 2, 4, 2, 5};
-    int[] supoja3Pattern = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-
-    // 각 수포자가 맞춘 개수 계산
-    for (int i = 0; i < answers.length; i++) {
-      if (answers[i] == supoja1Pattern[i % supoja1Pattern.length]) count1++;
-      if (answers[i] == supoja2Pattern[i % supoja2Pattern.length]) count2++;
-      if (answers[i] == supoja3Pattern[i % supoja3Pattern.length]) count3++;
+    public int[] solution(int[] answers) {
+        int[] pattern1 = {1, 2, 3, 4, 5};
+        int[] pattern2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] pattern3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        
+        // 정답 개수
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0;
+        
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == pattern1[i % pattern1.length]) {
+                count1++;
+            }
+            if (answers[i] == pattern2[i % pattern2.length]) {
+                count2++;
+            }
+            if (answers[i] == pattern3[i % pattern3.length]) {
+                count3++;
+            }
+        }
+        
+        int max = Math.max(count1, Math.max(count2, count3));
+        List<Integer> list = new ArrayList<>();
+        
+        if (count1 == max) list.add(1);
+        if (count2 == max) list.add(2);
+        if (count3 == max) list.add(3);
+        
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
-
-    int maxCount = Math.max(count1, Math.max(count2, count3));
-      
-    List<Integer> result = new ArrayList<>();
-
-    if (maxCount == count1) result.add(1);
-    if (maxCount == count2) result.add(2);
-    if (maxCount == count3) result.add(3);
-
-    return result.stream().mapToInt(i -> i).toArray();
-  }
 }
