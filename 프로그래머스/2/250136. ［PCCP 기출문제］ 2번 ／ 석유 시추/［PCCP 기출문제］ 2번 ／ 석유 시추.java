312271ca0b9aa6073ba88661ad9f99f07d08669e
이선오 <140589023+oneseel@users.noreply.github.com>
2024-07-20 11:00:18 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 89.14 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -9,8 +9,9 @@ class Solution {
         n = land.length;
         m = land[0].length;
         visited = new boolean[n][m];
-        oil = new int[m];
+        oil = new int[m]; // 각 열에 따른 기름 양
         
+        // 방문하지 않은 석유 찾기
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (land[i][j] == 1 && !visited[i][j]) {
@@ -19,14 +20,16 @@ class Solution {
             }
         }
 
+        // oil 배열에서 최댓값 반환
         return Arrays.stream(oil).max().getAsInt();   
     }
     
+    // 석유의 양을 찾는 bfs 메서드
     private void bfs (int sx, int sy, int[][] land) {
         int[] dx = {1, -1, 0, 0};
         int[] dy = {0, 0, 1, -1};
         int count = 1;
-        Set<Integer> set = new HashSet<>();
+        Set<Integer> set = new HashSet<>(); // 석유가 있는 열을 저장
         
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[] {sx, sy});
@@ -50,11 +53,13 @@ class Solution {
             }
         }
         
+        // 각 열에 따른 석유량 계산
         for (int i : set) {
             oil[i] += count;
         }
     }
     
+    // land 범위 안인지 방문하지 않은 석유인지 확인하는 메서드
     private boolean check (int nx, int ny, int[][] land) {
         return (0 <= nx && nx < n && 0 <= ny && ny < m && !visited[nx][ny] && land[nx][ny] == 1);
     }
